02362169b3fcc97ccfb8170e0c6a99d280037d2c
arun-murali0 <arunkarthick0810@gmail.com>
2024-07-19 12:45:30 +0530

    routes added

--- server/src/routes/authRoute.ts
+++ server/src/routes/authRoute.ts
@@ -1,11 +1,15 @@
+/* This TypeScript code snippet is setting up a router using the Express framework. It imports */
+
 import { Router } from 'express';
 import { createNewUser, userLogin } from '../controller/index';
 import '../passport-strategy/local-passport';
 import passport from 'passport';
 
 const router = Router();
-
+// create new user
 router.post('/newuser', createNewUser);
+
+// login
 router.post('/login', passport.authenticate('local'), userLogin);
 
 export default router;
--- server/src/routes/index.ts
+++ server/src/routes/index.ts
@@ -1,8 +1,11 @@
+// index router
+
 import { Router } from 'express';
 import AuthRoute from './authRoute';
 
 const router = Router();
 
+// auth router 
 router.use('/api/auth', AuthRoute);
 
 export default router;
