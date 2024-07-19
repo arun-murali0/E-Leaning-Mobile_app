f5ec16281ef897d9273f3865bcfe283aec01bad6
arun-murali0 <arunkarthick0810@gmail.com>
2024-07-19 13:00:17 +0530

    login session complted throgh passport js

--- server/src/controller/auth/userLogin.ts
+++ server/src/controller/auth/userLogin.ts
@@ -2,10 +2,13 @@
 import { Request, Response } from 'express';
 import { User } from '../../model/user';
 import { tryCatch } from '../../middleware/tryCatch';
+import { CustomError } from '../../middleware/errorHandler';
 // import { SET_DATA, GET_DATA } from '../../config/database';
 
 export const userLogin = tryCatch(async (req: Request, res: Response) => {
   const { email, password } = req.body;
   const user = await User.findOne({ email: email }).exec();
+  if (!user) throw new CustomError('user Not Found', 400);
+
   res.status(200).json({ response: 'success', message: 'login successfull' });
 });
